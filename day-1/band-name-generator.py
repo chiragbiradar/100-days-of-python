cff95305f2a77b7080950f7ebcf17dce9d6527cd
Chirag Biradar <chiragsb16@gmail.com>
2023-01-27 22:29:06 +0530

    Day-1 project added

---
+++ day-1/band-name-generator.py
@@ -0,0 +1,7 @@
+print("Welcome to the band name generator")
+
+city_name = input("What is the city you grew up in?\n")
+
+pet_name = input("What is the name of your pet?\n")
+
+print("Your band name can be " + city_name+ " " + pet_name + "\n")
